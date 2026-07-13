51f0781bf43fa7fc1f4ca24f4d282c0667240c37
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-07-13 03:26:38 +0300

    Define vertual network and subnets.

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_virtual_network" "this" {
+  name                = "vnet-formbricks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet_address_space
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "snet-aks"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.aks_subnet_prefix]
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = "snet-db"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.db_subnet_prefix]
+
+  delegation {
+    name = "db-delegation"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
+}
