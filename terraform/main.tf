a55428b26790a6280bd3f1db9538d62d5edc8f26
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-07-14 21:30:38 +0300

    Call network module.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 resource "azurerm_resource_group" "formbricks" {
-  name     = "rg-formbricks"
-  location = "qatarcentral"
+  name     = "rg-${var.project}-${var.environment}"
+  location = var.location
+  tags     = var.tags
+}
+
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.formbricks.name
+  location            = azurerm_resource_group.formbricks.location
+  tags                = var.tags 
 }
