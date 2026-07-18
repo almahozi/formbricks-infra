82e2cac7941bab6169c3215e0949fbd0803f1bd3
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-07-18 21:37:03 +0300

    Define k8s cluster resource.

---
+++ terraform/modules/aks/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_kubernetes_cluster" "formbricks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+  tags                = var.tags
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
